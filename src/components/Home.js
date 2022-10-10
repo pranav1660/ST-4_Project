12d1adb020d160bf43023abfdb9866f54f5be299
pranav1660 <78558642+pranav1660@users.noreply.github.com>
2022-10-11 00:46:11 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './HomeStyles.css'
+import Navbar from './Navbar'
+import Hero from './Hero'
+import Pricing from './Pricing'
+import Testimonials from './Testimonials'
+import Faq from './Faq'
+import { Contact } from './Contact'
+import Footer from './Footer'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <Pricing />
+            <Faq />
+            <Contact />
+            <Footer/>
+        </div>
+    )
+}
+
+export default Home
